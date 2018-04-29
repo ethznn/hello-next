dd41aeda8b4b99c7f94101f645896a23ef4300cf
prayeo <prxyeo@gmail.com>
2018-04-29 16:29:58 +0900

    yarn run start

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,16 @@
 import Layout from '../components/Layout';
 
 const About = () => (
-    <Layout>
-        <h2>안녕하세요 저는 Velopert 입니다.</h2>
-    </Layout>
+  <Layout>
+    <style jsx>
+      {`
+        h2 {
+          color: grey
+        }
+      `}
+    </style>
+    <h2>안녕하세요 저는 Velopert 입니다.</h2>
+  </Layout>
 )
 
 export default About;
---
+++ pages/next.config.js
@@ -0,0 +1,9 @@
+// This file is not going through babel transformation.
+// So, we write it in vanilla JS
+// (But you could use ES2015 features supported by your Node.js version)
+
+export function webpack(config, { dev }) {
+  // Perform customizations to config
+  // Important: return the modified config
+  return config;
+}
